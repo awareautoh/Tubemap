d797ce95e3d5ad36b587628e38152d666e9322e1
awareautoh <ouphachay@hotmail.com>
2018-06-15 22:40:38 +0100

    first comit

---
+++ app/src/main/java/com/example/awareautoh/londontubemapoffline/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.awareautoh.londontubemapoffline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.chrisbanes.photoview.PhotoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
+        photoView.setImageResource(R.drawable.tubemap1);
+
+
+
+    }
+}
